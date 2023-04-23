513d37f07d1470861dc0942846ee46b36e793c3f
Alex Almeida <alex.almeida@almerati.com>
2023-04-22 22:15:55 -0400

    hasta la tarea #9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -49,8 +49,44 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+    const comment = req.body.comment;
+    let book = books[isbn];
+    if(book) {
+        keys = Object.keys(book.reviews);
+        let message = "Review successfully "
+        if ((keys.length > 0) && (book.reviews[username])) {
+            message += "updated";
+        } else {
+            message += "added";
+        }
+        book.reviews[username] = comment;
+        res.send(message);
+    } else {
+        res.send("Unable to find book!")
+    }
+});
+
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+    let book = books[isbn];
+    if(book) {
+        keys = Object.keys(book.reviews);
+        let message = ""
+        if ((keys.length > 0) && (book.reviews[username])) {
+            message += "Review successfully deleted";
+        } else {
+            message += "You can only delete your own reviews";
+        }
+        delete book.reviews[username];
+        res.send(message);
+    } else {
+        res.send("Unable to find book!")
+    }
 });
 
 module.exports.authenticated = regd_users;
